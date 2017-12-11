02ad63a3e8eb906acf67e45a500e1159ec884067
abdilatifbashir <abdiladifbashir0000@gmail.com>
2017-12-11 20:27:34 +0300

    add squad form

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -24,6 +24,12 @@ public class App {
     return new ModelAndView(model, layout);
   }, new VelocityTemplateEngine());
 
+  get("/squads/new", (request, response) -> {
+       Map<String, Object> model = new HashMap<String, Object>();
+       model.put("template", "templates/squad-form.vtl");
+       return new ModelAndView(model, layout);
+     }, new VelocityTemplateEngine());
+
 }
 
 
