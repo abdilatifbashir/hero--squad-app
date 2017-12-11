5844663ea0a5a76b66711a7f17087b7ca91ee2ab
abdilatifbashir <abdiladifbashir0000@gmail.com>
2017-12-11 17:56:50 +0300

    add member variable of the hero in Hero.java file

---
+++ src/main/java/Hero.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Hero{
+  private String mName;
+  private int mAge;
+  private String mWeakness;
+  private String mPower;
+  private int mId;
+  private static List<Hero> instances = new ArrayList<Hero>();
