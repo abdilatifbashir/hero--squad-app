3b0f1e3fd1c73700e7251d9865f579ebe01c5c61
abdilatifbashir <abdiladifbashir0000@gmail.com>
2017-12-11 18:16:14 +0300

    add squad member properties,constructor and getter method to retun memeber properties

---
+++ src/main/java/Squad.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Squad{
+  private int mMaxSize;
+  private String mName;
+  private String mCause;
+  private static List<Squad> instances = new ArrayList<Squad>();
+  private int mId;
+  private List<Hero> mHeros;
+
+  
+  public Squad(int Maxsize, String Name, String Cause){
+     mMaxSize = Maxsize;
+     mName = Name;
+     mCause = Cause;
+     instances.add(this);
+     mId = instances.size();
+     mHeros = new ArrayList<Hero>();
+  }
+
+  public int getMaxSize(){
+    return mMaxSize;
+  }
+  public String getName(){
+    return mName;
+  }
+  public String getCause(){
+    return mCause;
+  }
+  public int getId(){
+    return mId;
+  }
+  public static List<Squad> all(){
+    return instances;
+  }
+  public static void clear(){
+    instances.clear();
+  }
+  public void addHero(hero){
+    mHeros.add(hero);
+  }
+  public List<Hero> getHeros() {
+   return mHeros;
+ }
+ public static Squad find(int id) {
+   return instances.get(id - 1);
+  }
+
+}
