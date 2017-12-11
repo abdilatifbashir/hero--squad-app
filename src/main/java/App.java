91e839a3f01170b4744dabba8bf97b90fd7dfd0a
abdilatifbashir <abdiladifbashir0000@gmail.com>
2017-12-11 18:24:11 +0300

    add home page root-----/

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -16,4 +16,12 @@ public class App {
 
     setPort(port);
 }
+
+      //Root that triggers my templates
+      get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
 }
