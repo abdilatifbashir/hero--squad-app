42d5226d2caf90bace59e811f0a4536aba076f5c
abdilatifbashir <abdiladifbashir0000@gmail.com>
2017-12-11 18:04:56 +0300

    create constructor with Hero characters as arguments and get assigned values by constructor by use of getter method

--- src/main/java/Hero.java
+++ src/main/java/Hero.java
@@ -8,3 +8,33 @@ public class Hero{
   private String mPower;
   private int mId;
   private static List<Hero> instances = new ArrayList<Hero>();
+  public Hero(String name, int age, String strength, String weakness){
+      mName = name;
+      mAge = age;
+      mPower = strength;
+      mWeakness =weakness ;
+      instances.add(this);
+      mId = instances.size();
+    }
+    public String getName(){
+      return mName;
+    }
+    public int getAge(){
+      return mAge;
+    }
+    public String getSpecialPower(){
+      return mPower;
+    }
+    public String getWeakness(){
+      return mWeakness;
+    }
+    public int getId(){
+      return mId;
+    }
+    public static List<Hero> all(){
+      return instances;
+    }
+    public static void clear(){
+      instances.clear();
+    }
+  }
